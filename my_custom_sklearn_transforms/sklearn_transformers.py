7c6c1e25e0f36265c81032040b31516b090f8a89
wladimir campos <wladimir09@gmail.com>
2020-08-25 11:41:08 -0400

    BalancedDatasetv2

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -17,21 +17,17 @@ class DropColumns(BaseEstimator, TransformerMixin):
     
 # All sklearn Transforms must have the `transform` and `fit` methods
 class BalancedDataset(BaseEstimator, TransformerMixin):
+    def __init__(self, minority):
+        self.minority = minority
+        
     def fit(self, X, y=None):
         return self
 
     def transform(self, X):
-        # Primeiro realizamos a cópia do dataframe 'X' de entrada
         data = X.copy()
-        # Separate majority and minority classes
-        data_majority = data[data.OBJETIVO=='Aceptado']
-        data_minority = data[data.OBJETIVO=='Sospechoso']
-        # Upsample minority class
-        data_minority_upsampled = resample(data_minority, 
-                                         replace=True,
-                                         n_samples=8000,
-                                         random_state=123)
-        # Combine majority class with upsampled minority class
-        data_upsampled = pd.concat([data_majority, data_minority_upsampled])
-        data_upsampled = pd.concat([data_upsampled, data_minority])
+        data_minority = resample(self.minority, 
+                                 replace=True,
+                                 n_samples=8000,
+                                 random_state=123)
+        data_upsampled = pd.concat([data, data_minority])
         return data_upsampled
